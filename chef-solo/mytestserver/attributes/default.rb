77d22e58ea163458be19c0380bc299f38e48ccf0
Marco Moscatiello <marco.moscatiello@icloud.com>
2017-10-04 17:18:49 +0100

    added java install

--- chef-solo/mytestserver/attributes/default.rb
+++ chef-solo/mytestserver/attributes/default.rb
@@ -8,5 +8,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-default['mydrive-elasticsearch-server']['java']['version'] = '8u131-b11-2ubuntu1.16.04.2'
-default['mydrive-elasticsearch-server']['elasticsearch']['version'] = '5.5.0'
+
+default['elasticsearch-server']['java']['version'] = '8u131-b11-2ubuntu1.16.04.3'
+default['elasticsearch-server']['elasticsearch']['version'] = '5.5.0'
+default['elasticsearch-server']['elasticsearch']['cluster-name'] = 'elasticsearch'
--- chef-solo/mytestserver/recipes/install.rb
+++ chef-solo/mytestserver/recipes/install.rb
@@ -9,3 +9,23 @@
 
 package 'htop'
 
+package 'openjdk-8-jdk' do
+  version "#{node['elasticsearch-server']['java']['version']}"
+end
+
+apt_repository 'elasticsearch' do
+  uri          'https://artifacts.elastic.co/packages/5.x/apt'
+  distribution 'stable'
+  components   ['main']
+  key          'D88E42B4'
+end
+
+package 'elasticsearch' do
+  version "#{node['elasticsearch-server']['elasticsearch']['version']}"
+end
+
+# execute 'install-ec2discovery-plugin' do
+#   command './elasticsearch-plugin install discovery-ec2'
+#   not_if  '/usr/share/elasticsearch/bin/elasticsearch-plugin list | grep discovery-ec2'
+#   cwd     '/usr/share/elasticsearch/bin/'
+# end
