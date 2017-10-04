dade4f86227685a8321f180c93adae0df8c569a6
Marco Moscatiello <marco.moscatiello@icloud.com>
2017-10-04 16:05:56 +0100

    added test server for chef

---
+++ chef-solo/mytestserver/metadata.rb
@@ -0,0 +1,7 @@
+name             'mytestserver'
+maintainer       'Marco Moscatiello'
+maintainer_email 'marco.moscatiello@icloud.com'
+license          'All rights reserved'
+description      'Installs/Configures mytestserver'
+long_description 'Installs/Configures mytestserver'
+version          '0.1.0'
