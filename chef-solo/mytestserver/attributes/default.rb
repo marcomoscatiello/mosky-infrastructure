105676b4f008c5bd0b70cf3fa4b98966b8d5aa91
Marco Moscatiello <marco.moscatiello@icloud.com>
2017-10-04 16:16:15 +0100

    added htop

---
+++ chef-solo/mytestserver/attributes/default.rb
@@ -0,0 +1,12 @@
+#
+# Attributes:: mytestserver
+# Recipe:: default
+#
+#
+# Copyright (C) 2017 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+default['mydrive-elasticsearch-server']['java']['version'] = '8u131-b11-2ubuntu1.16.04.2'
+default['mydrive-elasticsearch-server']['elasticsearch']['version'] = '5.5.0'
