5f86f9bea0af0641f53caf14b52bda00c0563371
Marco Moscatiello <marco.moscatiello@icloud.com>
2019-12-13 13:25:57 +0000

    working

--- tools/install-ohmyzsh-agnoster.sh
+++ tools/install-ohmyzsh-agnoster.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
+#echo "Oh-My-Zsh + Agnoster theme :"
+#echo "1: Install"
+#echo "2: Uninstall"
+#echo "3: Abort"
+#read -p "Select option [1 | 2 | 3 ] " CONT
+#if [ "$CONT" = "1" ] ; then
+
+#elif [ "$CONT = "2" ] ; then
+
+#else "Operation Aborted. " ;
+#fi
+
 echo "This script will install oh-my-zsh + agnoster for git and iterm2"
 read -p "Continue (y/n)?" CONT
 if [ "$CONT" = "y" ]; then
   echo "installing oh-my-zsh.." ;
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" ;
-  
-  echo "installing powerline fonts for agnoster theme.."
-  mkdir -p temp-agnoster && cd "$_" ;
-  git clone https://github.com/powerline/fonts.git --depth=1 temp-agnoster ; ./temp-agnoster/fonts/install.sh ;
-  rm -rf temp-agnoster ;
+
+  if [ ! -e "$HOME/Library/Fonts/Meslo LG M Regular for Powerline.ttf" ] ; then
+    printf "Powerline font not installed. "
+    echo "installing powerline fonts for agnoster theme.."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/powerline/fonts/master/install.sh)"
+
+  else echo "Powerline fonts already installed."
+  fi  
+
   echo "modifying the theme to agnoster.."
   sed -i -e 's/robbyrussell/agnoster/g' ~/.zshrc ; 
   echo "modifying the agnoster configuration in this path.."
